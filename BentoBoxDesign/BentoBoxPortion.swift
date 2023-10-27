cdc32c80a3eda173ef5b79e6c76a4d159b7ee65a
Lexi Harrop <lharrop@lcs.on.ca>
2023-10-27 09:58:11 -0400

    I split most of the layout into their respective portions preparing to add all text and images

---
+++ BentoBoxDesign/BentoBoxPortion.swift
@@ -0,0 +1,18 @@
+//
+//  BentoBoxPortion.swift
+//  BentoBoxDesign
+//
+//  Created by Lexi on 2023-10-27.
+//
+
+import SwiftUI
+
+struct BentoBoxPortion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BentoBoxPortion()
+}
